b3161bdd419106c4e83a1958b7a440e492b1938f
Elitebook <Elitebook@DESKTOP-STJCPG5>
2023-10-25 18:26:00 +0300

    Add project files.

---
+++ prog.lab.1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PolynomialCalculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            double a1 = double.Parse(textBoxA1.Text);
+            double b1 = double.Parse(textBoxB1.Text);
+            double c1 = double.Parse(textBoxC1.Text);
+            double d1 = double.Parse(textBoxD1.Text);
+
+            double a2 = double.Parse(textBoxA2.Text);
+            double b2 = double.Parse(textBoxB2.Text);
+            double c2 = double.Parse(textBoxC2.Text);
+            double d2 = double.Parse(textBoxD2.Text);
+
+            Polynomial polynomial1 = new Polynomial(a1, b1, c1, d1);
+            Polynomial polynomial2 = new Polynomial(a2, b2, c2, d2);
+
+            Polynomial result = polynomial1.Add(polynomial2);
+
+            resultTextBlock.Text = $"Результат: {result}";
+        }
+    }
+}
---
+++ prog.lab.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+
+public class Polynomial
+{
+    public double A { get; set; }
+    public double B { get; set; }
+    public double C { get; set; }
+    public double D { get; set; }
+
+    public Polynomial(double a, double b, double c, double d)
+    {
+        A = a;
+        B = b;
+        C = c;
+        D = d;
+    }
+
+    public double Evaluate(double x)
+    {
+        return A * Math.Pow(x, 3) + B * Math.Pow(x, 2) + C * x + D;
+    }
+
+    public Polynomial Add(Polynomial other)
+    {
+        return new Polynomial(A + other.A, B + other.B, C + other.C, D + other.D);
+    }
+
+    public Polynomial Subtract(Polynomial other)
+    {
+        return new Polynomial(A - other.A, B - other.B, C - other.C, D - other.D);
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendFormat("{0}x^3", A);
+        if (B != 0) sb.AppendFormat(" + {0}x^2", B);
+        if (C != 0) sb.AppendFormat(" + {0}x", C);
+        if (D != 0) sb.AppendFormat(" + {0}", D);
+        return sb.ToString();
+    }
+}
